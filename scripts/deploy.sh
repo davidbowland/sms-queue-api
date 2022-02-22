b01e1827c1e9577a9e95c8bb218e8298f68eac64
David Bowland <dbowland1@gmail.com>
2022-02-22 00:00:40 -0600

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,13 +14,14 @@ sam build --template ${SAM_TEMPLATE} --use-container
 
 # Deploy build lambda
 
-TESTING_ARTIFACTS_BUCKET=aws-sam-cli-managed-sms-queue-api-artifactsbucket-wcjhz27amg5c
-TESTING_CLOUDFORMATION_EXECUTION_ROLE="arn:aws:iam::$AWS_ACCOUNT_ID:role/aws-sam-cli-managed-sms-q-CloudFormationExecutionR-1G18VBAHID89D"
+TESTING_ARTIFACTS_BUCKET=dbowland-lambda-east-1-test
+TESTING_CLOUDFORMATION_EXECUTION_ROLE="arn:aws:iam::$AWS_ACCOUNT_ID:role/dbowland-cloudformation-test"
 TESTING_STACK_NAME=sms-queue-api-test
 sam deploy --stack-name ${TESTING_STACK_NAME} \
            --capabilities CAPABILITY_IAM \
            --region us-east-1 \
            --s3-bucket ${TESTING_ARTIFACTS_BUCKET} \
+           --s3-prefix sms-queue-api-test \
            --no-fail-on-empty-changeset \
            --role-arn ${TESTING_CLOUDFORMATION_EXECUTION_ROLE} \
-           --parameter-overrides "AccountId=$AWS_ACCOUNT_ID Environment=test"
+           --parameter-overrides "Environment=test"
