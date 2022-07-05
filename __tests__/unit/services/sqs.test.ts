3080eb36038b5c05f2e1e6ddd7f48d0a9e1f5237
David Bowland <dbowland1@gmail.com>
2022-07-05 01:17:31 -0500

    Implement AWS X-Ray

--- __tests__/unit/services/sqs.test.ts
+++ __tests__/unit/services/sqs.test.ts
@@ -7,6 +7,9 @@ jest.mock('aws-sdk', () => ({
     sendMessage: (...args) => ({ promise: () => mockSendMessage(...args) }),
   })),
 }))
+jest.mock('@utils/logging', () => ({
+  xrayCapture: jest.fn().mockImplementation((x) => x),
+}))
 
 describe('sqs', () => {
   describe('addToQueue', () => {
--- src/services/sqs.ts
+++ src/services/sqs.ts
@@ -2,8 +2,9 @@ import { SQS } from 'aws-sdk'
 
 import { sqsMessageGroupId, sqsQueueUrl } from '../config'
 import { SMSMessage } from '../types'
+import { xrayCapture } from '../utils/logging'
 
-const sqs = new SQS({ apiVersion: '2012-11-05' })
+const sqs = xrayCapture(new SQS({ apiVersion: '2012-11-05' }))
 
 /* Message queue */
 
